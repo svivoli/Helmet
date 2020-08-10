dab6ce22faf89a46607d478c09e6d31e6d5457e3
Sarah Vivoli <snvivoli@gmail.com>
2020-08-10 13:22:20 -0700

    heroku deploy

--- server.js
+++ server.js
@@ -28,15 +28,14 @@ app.use(function(req, res, next) {
 const routes = require('./routes');
 app.use(routes);
 
-if (process.env.NODE_ENV === 'production') {
-  // Express will serve up production assets
-  app.use(express.static('client/build'));
-
-  // Express serve up index.html file if it doesn't recognize route
-  app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 }
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 app.listen(PORT, function () {
     console.log(`🌎 ==> API server now on port ${PORT}!`);
