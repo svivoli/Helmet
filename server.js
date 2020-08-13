5624dd9571c07e42f904cf2138562d41aab6502f
Sarah Vivoli <snvivoli@gmail.com>
2020-08-13 11:14:57 -0700

    heroku push

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const PORT = process.env.PORT || 3002;
 const app = express();
 
-const MONGO_URI = process.env.MONGODB_URI || "mongodb://svivoli:password1@ds161640.mlab.com:61640/heroku_q379qz6c";
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://snvivoli:password1@ds231739.mlab.com:31739/heroku_0lz9s37f";
 // const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/helmet";
 const mongoose = require("mongoose");
 // const MongoStore = require("connect-mongo")(session);
@@ -19,24 +19,23 @@ app.use(express.json());
 
 // app.use(express.static(__dirname + '/public'));
 
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     next();
-//   })
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  })
 
 const routes = require('./routes');
 app.use(routes);
 
-if (process.env.NODE_ENV === 'production') {
-  // Express will serve up production assets
-  app.use(express.static('client/build'));
-
-  // Express serve up index.html file if it doesn't recognize route
-  app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 }
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 app.listen(PORT, function () {
     console.log(`🌎 ==> API server now on port ${PORT}!`);
