ee6a65731f3ae9f857973d8f7ab2c6f7f894224a
Sarah Vivoli <snvivoli@gmail.com>
2020-08-13 11:06:55 -0700

    heroku push

--- server.js
+++ server.js
@@ -19,11 +19,11 @@ app.use(express.json());
 
 // app.use(express.static(__dirname + '/public'));
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  })
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+//   })
 
 const routes = require('./routes');
 app.use(routes);
